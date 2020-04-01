1f8898292b4464695614379edc1604a91426fea5
Abdoul <abdoulniyigena@gmail.com>
2020-04-01 02:47:01 +0200

    make async request to server and render validation errors

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Message } from "semantic-ui-react";
 import Validator from "validator";
 import PropTypes from "prop-types";
 import InlineError from "../messages/InlineError";
@@ -14,14 +14,22 @@ class LoginForm extends React.Component {
     errors: {},
   };
 
-  onChange = (event) =>
+  onChange = (e) =>
     this.setState({
-      data: { ...this.state.data, [event.target.name]: event.target.value },
+      data: { ...this.state.data, [e.target.name]: e.target.value },
     });
 
   onSubmit = () => {
     const errors = this.validate(this.state.data);
     this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
+      this.setState({ loading: true });
+      this.props
+        .submit(this.state.data)
+        .catch((err) =>
+          this.setState({ errors: err.response.data.errors, loading: false })
+        );
+    }
   };
 
   validate = (data) => {
@@ -35,18 +43,20 @@ class LoginForm extends React.Component {
       errors.password = "password can't be blank";
     }
 
-    if (Object.keys(errors).length === 0) {
-      this.props.submit(this.state.data);
-    }
-
     return errors;
   };
 
   render() {
-    const { data, errors } = this.state;
+    const { data, errors, loading } = this.state;
 
     return (
-      <Form onSubmit={this.onSubmit}>
+      <Form onSubmit={this.onSubmit} loading={loading}>
+        {errors.global && (
+          <Message negative>
+            <Message.Header>Something went wrong</Message.Header>
+            <p>{errors.global}</p>
+          </Message>
+        )}
         <Form.Field error={!!errors.email}>
           <label htmlFor="email">Email</label>
           <input
