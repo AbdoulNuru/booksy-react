cf55e86f51d0b5ab421ea8974ecd20ae5da926d6
Abdoul <abdoulniyigena@gmail.com>
2020-04-07 17:37:17 +0200

    create user dashboard and authorized routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import PropTypes from "prop-types";
 import Homepage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
+import DashoardPage from "./components/pages/DashboardPage";
+import UserRoute from "./components/routes/UserRoute";
+import GuestRoute from "./components/routes/GuestRoute";
 
-const App = () => (
+const App = ({ location }) => (
   <div className="ui container">
-    <Route path="/" exact component={Homepage} />
-    <Route path="/login" exact component={LoginPage} />
+    <Route location={location} path="/" exact component={Homepage} />
+    <GuestRoute location={location} path="/login" exact component={LoginPage} />
+    <UserRoute
+      location={location}
+      path="/dashboard"
+      exact
+      component={DashoardPage}
+    />
   </div>
 );
+
+App.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default App;
